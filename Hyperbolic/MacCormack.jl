0ef34e527e59a837dda8fd71730febb7edd4523c
sauroncool <ambujiist@gmail.com>
2023-03-29 04:12:54 +0530

    Added

--- Hyperbolic/MacCormack.jl
+++ Hyperbolic/MacCormack.jl
@@ -1,28 +1,27 @@
 using Plots
 plotly(ticks=:native) # Allow to zoom and will adjust the grid
 
-function predictor(u, c)    # Lax Friedrich Method
+function predictor(u, c) # FTFS Method
     v = copy(u)
-    v[1] = (u[2] + u[end]) / 2 - (c / 4) * (u[2] - u[end])
-    v[2:end-1] .= ((u[3:end] .+ u[1:end-2]) ./ 2) .- (c / 4) .* (u[3:end] .- u[1:end-2])
-    v[end] = (u[1] + u[end-1]) / 2 - (c / 4) * (u[1] - u[end-1])
+    v[1:end-1] .= u[1:end-1] .- c .* (u[2:end] .- u[1:end-1])
+    v[end] = u[end] - c * (u[1] - u[end])
     return v
 end
 
-function corrector(u, u_prev, c)    # Leap Frog Method
+function corrector(u, u_prev, c)    # BTBS Method
     v = copy(u)
-    v[1] = u_prev[1] - (c / 2) * (u[2] - u[end])
-    v[2:end-1] .= u_prev[2:end-1] .- (c / 2) .* (u[3:end] .- u[1:end-2])
-    v[end] = u_prev[end] - (c / 2) * (u[1] - u[end-1])
+    v[1] = ((u_prev[1] + u[1]) / 2) - (c / 2) * (u[1] - u[end])
+    v[2:end] .= ((u_prev[2:end] .+ u[2:end]) ./ 2) .- (c / 2) .* (u[2:end] .- u[1:end-1])
     return v
 end
 
-function Richtmyer(u, c)
+function MacCormack(u, c)
     u_prev = copy(u)
     u_predicted = predictor(u, c)
     u_corrected = corrector(u_predicted, u_prev, c)
     return u_corrected
 end
+
 # Define the grid parameters
 L = 10.0   # Length of the domain in the x direction 
 Δx = 0.01  # Grid spacing in the x direction 
@@ -40,26 +39,23 @@ num_time_step = round(sim_time / Δt)   # Number of time steps
 # Define the initial condition
 x_values = range(0, stop=L, length=Nx)
 u = exp.(-4 .* (x_values .- 5) .^ 2)
-
 # Plot the initial condition
-plot(xlabel="x", ylabel="Amplitude", title="Richtmyer", legend=:topleft, grid=true)
+plot(xlabel="x", ylabel="Amplitude", title="MacCormack", legend=:topleft, grid=true)
 plot!(x_values, u, label="Initial Condition")
-
 # Run the simulation
 for j in 1:num_time_step
-    global u = Richtmyer(u, c)
+    global u = MacCormack(u, c)
 end
 
 # Numerical
 plot!(x_values, u, label="After $(sim_time) seconds (numerically)")
-
 # Define the simulation parameters
 sim_time_2 = 6   # Total simulation time 
 num_time_step_2 = round(sim_time_2 / Δt)   # Number of time steps
 
 # Run the simulation
 for j in 1:num_time_step_2
-    global u = Richtmyer(u, c)
+    global u = MacCormack(u,c)
 end
 
 # Numerical

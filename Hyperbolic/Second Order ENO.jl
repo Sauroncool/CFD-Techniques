0e56e94fadbdada6a6d249c1601b0893dfe41a7f
sauroncool <ambujiist@gmail.com>
2023-04-04 22:03:57 +0530

    Edited

--- Hyperbolic/Second Order ENO.jl
+++ Hyperbolic/Second Order ENO.jl
@@ -1,26 +1,26 @@
 using Plots
 plotly(ticks=:native) # Allow to zoom and will adjust the grid
 
-# function ENO(u, Δt, α, Δx)
-#     v = copy(u)
-#     v[1] = u[1] - ((Δt * α) * (((u[1] - u[end]) / Δx) + (Δx / 2) * minmod((u[2] - 2 * u[1] + u[end]) / (Δx^2), (u[1] - 2 * u[end] + u[end-1]) / (Δx^2))))
-#     v[2] = u[2] - ((Δt * α) * (((u[2] - u[1]) / Δx) + (Δx / 2) * minmod((u[3] - 2 * u[2] + u[1]) / (Δx^2), (u[2] - 2 * u[1] + u[end]) / (Δx^2))))
-#     v[3:end-1] .= u[3:end-1] .- ((Δt * α) .* (((u[3:end-1] .- u[2:end-2]) / Δx) .+ (Δx / 2) * minmod.((u[4:end] - 2 * u[3:end-1] + u[2:end-2]) / (Δx^2), (u[3:end-1] - 2 * u[2:end-2] + u[1:end-3]) / (Δx^2))))
-#     v[end] = u[end] - ((Δt * α) * (((u[end] - u[end-1]) / Δx) + (Δx / 2) * minmod((u[1] - 2 * u[end] + u[end-1]) / (Δx^2), (u[end] - 2 * u[end-1] + u[end-2]) / (Δx^2))))
-#     return v
-# end
-
 function ENO(u, Δt, α, Δx)
-    n=length(u)
-    v = similar(u)
-    for i = 1:n
-        in1 = mod1(i - 1, n)    # index of i-1 with periodic boundary
-        in2 = mod1(i - 2, n)    # index of i-2 with periodic boundary
-        ip1 = mod1(i + 1, n)    # index of i+1 with periodic boundary
-        v[i] = u[i] - ((Δt * α) * (((u[i] - u[in1]) / Δx) + (Δx / 2) * minmod((u[ip1] - 2 * u[i] + u[in1]) / (Δx^2), (u[i] - 2 * u[in1] + u[in2]) / (Δx^2))))
-    end
+    v = copy(u)
+    v[1] = u[1] - ((Δt * α) * (((u[1] - u[end]) / Δx) + (Δx / 2) * minmod((u[2] - 2 * u[1] + u[end]) / (Δx^2), (u[1] - 2 * u[end] + u[end-1]) / (Δx^2))))
+    v[2] = u[2] - ((Δt * α) * (((u[2] - u[1]) / Δx) + (Δx / 2) * minmod((u[3] - 2 * u[2] + u[1]) / (Δx^2), (u[2] - 2 * u[1] + u[end]) / (Δx^2))))
+    v[3:end-1] .= u[3:end-1] .- ((Δt * α) .* (((u[3:end-1] .- u[2:end-2]) / Δx) .+ (Δx / 2) * minmod.((u[4:end] - 2 * u[3:end-1] + u[2:end-2]) / (Δx^2), (u[3:end-1] - 2 * u[2:end-2] + u[1:end-3]) / (Δx^2))))
+    v[end] = u[end] - ((Δt * α) * (((u[end] - u[end-1]) / Δx) + (Δx / 2) * minmod((u[1] - 2 * u[end] + u[end-1]) / (Δx^2), (u[end] - 2 * u[end-1] + u[end-2]) / (Δx^2))))
     return v
 end
+
+# function ENO(u, Δt, α, Δx)
+#     n=length(u)
+#     v = similar(u)
+#     for i = 1:n
+#         in1 = mod1(i - 1, n)    # index of i-1 with periodic boundary
+#         in2 = mod1(i - 2, n)    # index of i-2 with periodic boundary
+#         ip1 = mod1(i + 1, n)    # index of i+1 with periodic boundary
+#         v[i] = u[i] - ((Δt * α) * (((u[i] - u[in1]) / Δx) + (Δx / 2) * minmod((u[ip1] - 2 * u[i] + u[in1]) / (Δx^2), (u[i] - 2 * u[in1] + u[in2]) / (Δx^2))))
+#     end
+#     return v
+# end
 
 
 function minmod(p, q)

89501f65ae27f9cddb0afc635259f524504e1ec8
sauroncool <ambujiist@gmail.com>
2023-04-05 18:34:54 +0530

    Added Q2

--- Hyperbolic/Richtmyer.jl
+++ Hyperbolic/Richtmyer.jl
@@ -39,7 +39,8 @@ num_time_step = round(sim_time / Δt)   # Number of time steps
 
 # Define the initial condition
 x_values = range(0, stop=L, length=Nx)
-u = exp.(-4 .* (x_values .- 5) .^ 2)
+#u = exp.(-4 .* (x_values .- 5) .^ 2)
+u = exp.(-30 .* (x_values .- 2) .^ 2) + exp.(-(x_values .- 5) .^ 2)
 
 # Plot the initial condition
 plot(xlabel="x", ylabel="Amplitude", title="Richtmyer", legend=:outertopleft, grid=true)
@@ -66,14 +67,19 @@ end
 plot!(x_values, u, label="After $(sim_time+sim_time_2) seconds (numerically)")
 
 # Analytical
-function Analytical(x,t)
-    peak_point=5+α*t
-    while (peak_point>=10)
-        peak_point=peak_point-10
+function Analytical(x, t)
+    peak_point = 5 + α * t
+    while (peak_point >= 10)
+        peak_point = peak_point - 10
     end
-    return exp(-4(x-peak_point)^2)+exp(-4(x-(peak_point+10))^2)
+    peak_point_2 = 2 + α * t
+    while (peak_point_2 >= 10)
+        peak_point_2 = peak_point_2 - 10
+    end
+    return exp(-30(x - peak_point_2)^2) + exp(-30(x - (peak_point_2 + 10))^2) + exp(-(x - peak_point)^2) + exp(-(x - (peak_point + 10))^2)
+    # return exp(-4(x-peak_point)^2)+exp(-4(x-(peak_point+10))^2)
 end
 
 plot!(0:Δx:10, Analytical.(0:Δx:10, sim_time), label="After $(sim_time) seconds (analytically)")
-plot!(0:Δx:10, Analytical.(0:Δx:10, sim_time+sim_time_2), label="After $(sim_time+sim_time_2) seconds (analytically)")
+plot!(0:Δx:10, Analytical.(0:Δx:10, sim_time + sim_time_2), label="After $(sim_time+sim_time_2) seconds (analytically)")
 # png("1_Richtmyer")
